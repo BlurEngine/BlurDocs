f9bc2fc56d7609079c25b00d5fb3e9caf6aa0ac8
SupaHam <supaham@SupaHam.com>
2016-02-17 17:45:02 -0500

    Create 404.jade and handle 404s properly.

--- routes/index.js
+++ routes/index.js
@@ -18,24 +18,46 @@ var express = require('express');
 var router = express.Router();
 var md = require('marked');
 
+function handle404(res, err, html, next) {
+    if (err) {
+        console.log(err);
+        console.log(Object.keys(err));
+        if(err['path']) { // Rendering error
+            next(err);
+        } else {          // 404
+            next();
+        }
+    } else {
+        res.send(html);
+    }
+}
+
 /* insert routes here. */
-router.use('/modules*', function(req, res, next) {
+router.use(/\/modules(\/.*)?/i, function(req, res, next) {
+    if (req.params[0] === undefined) req.params[0] = "";
     var path = 'modules' + req.params[0];
-    res.render(path, {md:md, path: "/" + path.replace(/\/+$/, "")});
+    res.render(path, {md:md, path: "/" + path.replace(/\/+$/, "")}, (err, html) => handle404(res, err, html, next));
 });
-router.use('/guides*', function(req, res, next) {
+router.use(/\/guides(\/.*)?/i, function(req, res, next) {
+    if (req.params[0] === undefined) req.params[0] = "";
     var path = 'guides' + req.params[0];
-    res.render(path, {md:md, path: "/" + path.replace(/\/+$/, "")});
+    res.render(path, {md:md, path: "/" + path.replace(/\/+$/, "")}, (err, html) => handle404(res, err, html, next));
 });
-router.use('/examples*', function(req, res, next) {
+router.use(/\/examples(\/.*)?/i, function(req, res, next) {
+    if (req.params[0] === undefined) req.params[0] = "";
     var path = 'examples' + req.params[0];
-    res.render(path, {md:md, path: "/" + path.replace(/\/+$/, "")});
+    res.render(path, {md:md, path: "/" + path.replace(/\/+$/, "")}, (err, html) => handle404(res, err, html, next));
 });
 router.use('/types', function(req, res, next) {
     res.render('types', {md:md, path: '/types'});
 });
-router.use('/', function(req, res, next) {
+router.use(/^\/$/, function(req, res, next) {
     res.render('index', {md:md, path: '/'});
+});
+
+// catch 404 and forward to error handler
+router.use(function(req, res, next) {
+    res.render('404', {req: req});
 });
 
 module.exports = router;
